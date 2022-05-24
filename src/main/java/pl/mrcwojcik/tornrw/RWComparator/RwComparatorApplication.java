cefe645b44b62fa7510075c0b9ba03370ae3b437
Marcin <maethi@gmail.com>
2022-05-24 17:59:34 +0200

    Basic maven project

---
+++ src/main/java/pl/mrcwojcik/tornrw/RWComparator/RwComparatorApplication.java
@@ -0,0 +1,13 @@
+package pl.mrcwojcik.tornrw.RWComparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RwComparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mrcwojcik/tornrw/RWComparator/RwComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mrcwojcik.tornrw.RWComparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
